51ccca29f0c352d437684f45fa9262bf3c287c2d
Davida Marion <davida.marion@gsa.gov>
2022-03-25 13:40:36 -0400

    Update tests to run in headless browser

--- spec/opportunity_explorer_results_spec.rb
+++ spec/opportunity_explorer_results_spec.rb
@@ -1,33 +1,32 @@
 require "spec_helper"
 
-describe "/opportunity-explorer-results" do
+describe "/opportunity-explorer-results", type: :feature, js: true do
 
-  # before :each do
-  #   visit "/opportunity-explorer/index.html"
-  # end
+  context "with no selections" do
+    before :each do
+      visit "/opportunity-explorer-results/index.html"
+    end
+    
+    it "renders" do
+      expect(page).to have_content "Your results"
+    end
 
-  it "has code 200" do
-    visit "/opportunity-explorer-results/index.html"
-    expect(page.status_code).to be(200)
-  end
+    it "has navigation links" do
+      expect(page).to have_link("General Services Administration", href: "/")
+      # TODO breadcrumbs
+    end
 
-  it "has navigation links" do
-    visit "/opportunity-explorer-results/index.html"
-    expect(page).to have_link("General Services Administration", href: "/")
-    # expect(page).to have_link("TODO breadcrumbs")
-  end
+    it "built answers from the YAML file" do
+      expect(page).to have_link("General Services Administration", href: "/")
 
-  it "built answers from the YAML file" do
-    visit "/opportunity-explorer-results/index.html"
+      expect(page).not_to have_content("Women-Owned Small Businesses")
+      expect(page).not_to have_css("div.setaside", count: 4)
+      expect(page).not_to have_css("#hubzone")
+      expect(page).not_to have_css("h3", text: "Historically Underutilized Business Zones (HUBZone)")
+      expect(page).not_to have_content("My business's principal office is located in a HUBZone")
 
-    puts page.body
-
-    expect(page).not_to have_css("div.setaside", count: 4)
-    expect(page).not_to have_css("#hubzone")
-    expect(page).not_to have_css("h3", text: "Historically Underutilized Business Zones (HUBZone)")
-    expect(page).not_to have_content("My business's principal office is located in a HUBZone")
-
-    # TODO add in programs
+      # TODO add in programs
+    end
   end
 
   context "when different setasides have been selected" do
