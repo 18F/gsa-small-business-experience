b0c4bbcc70e3448b73c6b80c26621aee12450760
JessicaVDussault <jessica.dussault@gsa.gov>
2022-04-14 12:04:30 -0500

    refactors js

--- _assets/js/opportunity-explorer.js
+++ _assets/js/opportunity-explorer.js
@@ -1,48 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
   const uri = new URL(window.location.href);
   const params = uri.searchParams;
-
-  // INDUSTRY
-
-  // fill in the industry label where required in the text based on the
-  // option selected in the quiz
-
   const industry = params.get("industry");
+  const revenue = params.get("revenue") == "1";
+  const setasides = params.getAll("setasides");
 
-  // search industries list until finding value == industry
-  // select the label
-  // fill in the label where required in the HTML
-  const selectedIndustry = industries.find(item => item.value == industry)
-  if (selectedIndustry) {
-    let label = selectedIndustry.label;
-    document
-      .querySelectorAll(".industry-name")
-      .forEach(item => item.innerHTML = "for \"" + label + "\"");
-  }
+  setIndustryLabel(industry);
+
   // start with all the options displayed, then hide non-relevant items
   // so that in the absence of JS users will be able to access the content
+  hideContracts(industry);
+  hidePrograms(industry, revenue);
+  hideSetAsides(setasides);
+});
 
-  // SETASIDES
+function checkRule(rule, param) {
+  // if the rule is null then this particular question does not apply
+  return (rule == null) ? true : (rule == param)
+}
 
-  // Hide all setasides that the user did not indicate were relevant to them
-
-  const setasides = params.getAll("setasides");
-  const setasideDivs = document.getElementById("setasides").children;
-
-  for (let ele of setasideDivs) {
-    if (!setasides.includes(ele.id)) {
-      ele.classList.add("display-none");
+// Hide any contracts which are not from the selected industry
+// If no industry is selected, hide all contracts
+function hideContracts(industry) {
+  if (industry == "") {
+    document.getElementById("contracts-table").classList.add("display-none");
+  } else {
+    let contractDivs = document.getElementById("contracts").children;
+    for (let row of contractDivs) {
+      if (!row.classList.contains("contract-" + industry)) {
+        row.classList.add("display-none");
+      }
     }
   }
+}
 
-  // PROGRAMS
-
-  // Use combinations of parameters to evaluate whether to display a program
-  // and hide all those that are not relevant to the selections
-
-  const revenue = params.get("revenue") == "1";
-  const purchase = params.get("purchase") == "1";
-
+// Use combinations or parameters to evaluate whether to display a program
+// and hide all those that are not relevant to the selections
+function hidePrograms(industry, revenue) {
   const programDivs = document.getElementById("programs").children;
 
   for (let program of programDivs) {
@@ -54,36 +48,37 @@ document.addEventListener("DOMContentLoaded", function(){
 
     if (alwaysDisplay) {
       // if the program is being displayed, check if its individual criteria
-      // for revenue and purchase have been met and if so, hide those warnings
+      // for revenue has been met and if so, hide the warning
       if (revenueMatch) { hideWarning("revenue", program.id); }
     } else if (!industryMatch || !revenueMatch) {
       program.classList.add("display-none");
     }
   }
+}
 
-  function checkRule(rule, param) {
-    // if the rule is null then this particular question does not apply
-    return (rule == null) ? true : (rule == param)
-  }
-
-  function hideWarning(type, programId) {
-    let warning = document.getElementById(programId + "-" + type);
-    if (warning) { warning.classList.add("display-none"); }
-  }
-
-  // CONTRACT EXAMPLES
-
-  // Hide any contracts which are not from the selected industry
-  // If no industry is selected, hide all contracts
-
-  if (industry == "") {
-    document.getElementById("contracts-table").classList.add("display-none");
-  } else {
-    contractDivs = document.getElementById("contracts").children;
-    for (let row of contractDivs) {
-      if (!row.classList.contains("contract-" + industry)) {
-        row.classList.add("display-none");
-      }
+// Hide all setasides that the user did not indicate were relevant to them
+function hideSetAsides(setasides) {
+  const setasideDivs = document.getElementById("setasides").children;
+  for (let ele of setasideDivs) {
+    if (!setasides.includes(ele.id)) {
+      ele.classList.add("display-none");
     }
   }
-});
+}
+
+function hideWarning(type, programId) {
+  let warning = document.getElementById(programId + "-" + type);
+  if (warning) { warning.classList.add("display-none"); }
+}
+
+// fill in the industry label where required in the text based on the
+// option selected in the quiz
+function setIndustryLabel(industry) {
+  const selectedIndustry = industries.find(item => item.value == industry)
+  if (selectedIndustry) {
+    let label = selectedIndustry.label;
+    document
+      .querySelectorAll(".industry-name")
+      .forEach(item => item.innerHTML = "for \"" + label + "\"");
+  }
+}
